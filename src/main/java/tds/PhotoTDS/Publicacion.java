9b8944d93ddc305c066ca91fb3141c5f8ff25758
oliatom350 <trollins030@gmail.com>
2022-11-23 12:03:54 +0100

    Creacion de clases vacias

---
+++ src/main/java/tds/PhotoTDS/Publicacion.java
@@ -0,0 +1,66 @@
+package tds.PhotoTDS;
+
+import java.util.*;
+
+public class Publicacion {
+
+	//Atributos
+	private String titulo;
+	private Date fecha;
+	private String descripcion;
+	private int meGusta;
+	private ArrayList<String> hashtags;
+	private ArrayList<Comentario> comentarios;
+	private String usuario;
+	
+	//Constructor
+	public Publicacion(String titulo, String descripcion, ArrayList<String> hashtags, String usuario) {
+		this.titulo = titulo;
+		this.descripcion = descripcion;
+		this.hashtags = hashtags;
+		
+		this.fecha = new Date();
+		this.meGusta = 0;
+		this.comentarios = new ArrayList<Comentario>();
+		this.usuario = usuario;
+	}
+	
+	//Metodos
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public int getMeGusta() {
+		return meGusta;
+	}
+
+	public ArrayList<String> getHashtags() {
+		return hashtags;
+	}
+
+	public ArrayList<Comentario> getComentarios() {
+		return comentarios;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+	
+	public void addMeGusta() {
+		this.meGusta++;
+	}
+	
+	public void addComentario (Comentario comentario) {
+		this.comentarios.add(comentario);
+	}
+	
+}
