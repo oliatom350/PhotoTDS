c376472732585f9dc6ad092fb7c9c53d871f3fc9
oliatom350 <trollins030@gmail.com>
2023-02-28 12:43:07 +0100

    VentanaPrincipal patron de profesor

--- src/main/java/tds/PhotoTDS/interfaz/VentanaPrincipal.java
+++ src/main/java/tds/PhotoTDS/interfaz/VentanaPrincipal.java
@@ -34,28 +34,19 @@ public class VentanaPrincipal extends JFrame {
 
 	private JPanel contentPane;
 	private static Usuario usuario;
-	private JPanel panel_ListFotos;
-	private JPanel panel;
-	private JScrollPane scrollPane;
-	private JPanel panelNorte;
-	private JPanel panelCentral2;
-	private JButton addFoto;
-	private JTextField textField;
-	private JPanel panelOeste2;
-	private JPanel panelEste2;
 	private JButton searchButton;
 	private Luz xmlLoaderButton;
+	private JPanel panelNorte;
+	private JPanel panelNorteCentral;
+	private JPanel panelNorteOeste;
+	private JPanel panelNorteEste;
 	private JLabel lblNewLabel;
-	private JLabel lblNewLabel1;
+	private JPanel panel;
 	private JPanel panel_1;
-	private JPanel panel_3;
-	private JPanel panel_4;
-	private JPanel panel_5;
-	private JButton btnNewButton;
-	private JButton btnNewButton_1;
 	private JLabel lblNewLabel_1;
+	private JPanel panel_2;
 	private JLabel lblNewLabel_2;
-	private JLabel lblNewLabel_3;
+	private JButton btnNewButton;
 
 	/**
 	 * Launch the application.
@@ -85,127 +76,93 @@ public class VentanaPrincipal extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
-		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
-		
-		JPanel panelSuperior = new JPanel();
-		contentPane.add(panelSuperior);
-		panelSuperior.setLayout(new BorderLayout(0, 0));
-				
-		panel_ListFotos = new JPanel();
-		
-		scrollPane = new JScrollPane(panel_ListFotos);
-		scrollPane.setPreferredSize(new Dimension(360, 220));
-		panelSuperior.add(scrollPane, BorderLayout.CENTER);
-		GridBagLayout gbl_panel_ListFotos = new GridBagLayout();
-		gbl_panel_ListFotos.columnWidths = new int[] {524, 0};
-		gbl_panel_ListFotos.rowHeights = new int[] {105, 105, 0};
-		gbl_panel_ListFotos.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		gbl_panel_ListFotos.rowWeights = new double[]{0.0, 0.0, 0.0};
-		panel_ListFotos.setLayout(gbl_panel_ListFotos);
-		
-		//Metemos el bucle
-		panel = new JPanel();
-		GridBagConstraints gbc_panel = new GridBagConstraints();
-		gbc_panel.fill = GridBagConstraints.HORIZONTAL;
-		gbc_panel.insets = new Insets(0, 0, 5, 0);
-		gbc_panel.gridx = 0;
-		gbc_panel.gridy = 0;
-		panel_ListFotos.add(panel, gbc_panel);
-		panel.setLayout(new BorderLayout(0, 0));
-		
-		lblNewLabel = new JLabel("");
-		panel.add(lblNewLabel, BorderLayout.WEST);
-		Image iconAddFoto = new ImageIcon(VentanaPrincipal.class.getResource("/images/arbol.png")).getImage().getScaledInstance(100, 100, DO_NOTHING_ON_CLOSE);
-		lblNewLabel.setIcon(new ImageIcon(iconAddFoto));
-		
-		panel_3 = new JPanel();
-		panel.add(panel_3, BorderLayout.CENTER);
-		panel_3.setLayout(new BoxLayout(panel_3, BoxLayout.Y_AXIS));
-		
-		panel_4 = new JPanel();
-		panel_3.add(panel_4);
-		
-		btnNewButton = new JButton("New button");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		panel_4.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
-		panel_4.add(btnNewButton);
-		
-		btnNewButton_1 = new JButton("New button");
-		panel_4.add(btnNewButton_1);
-		
-		lblNewLabel_1 = new JLabel("New label");
-		panel_4.add(lblNewLabel_1);
-		
-		panel_5 = new JPanel();
-		panel_3.add(panel_5);
-		panel_5.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
-		
-		lblNewLabel_2 = new JLabel("New label");
-		panel_5.add(lblNewLabel_2);
-		
-		lblNewLabel_3 = new JLabel("New label");
-		panel_5.add(lblNewLabel_3);
-		
-		panel_1 = new JPanel();
-		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
-		gbc_panel_1.fill = GridBagConstraints.HORIZONTAL;
-		gbc_panel_1.gridx = 0;
-		gbc_panel_1.gridy = 1;
-		panel_ListFotos.add(panel_1, gbc_panel_1);
-		panel_1.setLayout(new BorderLayout(0, 0));
-		
-		
-		lblNewLabel1 = new JLabel("");
-		panel_1.add(lblNewLabel1, BorderLayout.WEST);
-		iconAddFoto = new ImageIcon(VentanaPrincipal.class.getResource("/images/mausoleo.png")).getImage().getScaledInstance(100, 100, DO_NOTHING_ON_CLOSE);
-		lblNewLabel1.setIcon(new ImageIcon(iconAddFoto));
-		
-		JPanel panel_2 = new JPanel();
-		GridBagConstraints gbc_panel_2 = new GridBagConstraints();
-		gbc_panel_2.fill = GridBagConstraints.HORIZONTAL;
-		gbc_panel_2.gridx = 0;
-		gbc_panel_2.gridy = 2;
-		panel_ListFotos.add(panel_2, gbc_panel_2);
-		panel_2.setLayout(new BorderLayout(0, 0));
-		
-		JLabel lblNewLabel2 = new JLabel("");
-		panel_2.add(lblNewLabel2, BorderLayout.WEST);
-		iconAddFoto = new ImageIcon(VentanaPrincipal.class.getResource("/images/zeus.png")).getImage().getScaledInstance(100, 100, DO_NOTHING_ON_CLOSE);
-		lblNewLabel2.setIcon(new ImageIcon(iconAddFoto));
+		contentPane.setLayout(new BorderLayout(0, 0));
 		
 		panelNorte = new JPanel();
+		contentPane.add(panelNorte, BorderLayout.NORTH);
 		panelNorte.setLayout(new BorderLayout(0, 0));
-		panelSuperior.add(panelNorte, BorderLayout.NORTH);
 		
-		panelCentral2 = new JPanel();
-		panelNorte.add(panelCentral2, BorderLayout.CENTER);
+		panelNorteCentral = new JPanel();
+		panelNorte.add(panelNorteCentral, BorderLayout.CENTER);
 		
-		addFoto = new JButton("");
-		iconAddFoto = new ImageIcon(VentanaPrincipal.class.getResource("/images/iconoplus.png")).getImage();
+		panelNorteOeste = new JPanel();
+		panelNorte.add(panelNorteOeste, BorderLayout.WEST);
+		
+		panelNorteEste = new JPanel();
+		panelNorte.add(panelNorteEste, BorderLayout.EAST);
+		
+		JButton addFoto = new JButton("");
+		Image iconAddFoto = new ImageIcon(VentanaPrincipal.class.getResource("/images/iconoplus.png")).getImage();
 		addFoto.setIcon(new ImageIcon(iconAddFoto.getScaledInstance(20, 20, DO_NOTHING_ON_CLOSE)));
-		panelCentral2.add(addFoto);
+		panelNorteCentral.add(addFoto);
 		
-		textField = new JTextField();
+		JTextField textField = new JTextField();
 		textField.setColumns(10);
-		panelCentral2.add(textField);
+		panelNorteCentral.add(textField);
 		
 		searchButton = new JButton("");
 		iconAddFoto = new ImageIcon(VentanaPrincipal.class.getResource("/images/iconolupa.png")).getImage();
 		searchButton.setIcon(new ImageIcon(iconAddFoto.getScaledInstance(15, 15, DO_NOTHING_ON_CLOSE)));
-		panelCentral2.add(searchButton);
+		panelNorteCentral.add(searchButton);
 		
 		xmlLoaderButton = new Luz();
-		panelCentral2.add(xmlLoaderButton);
+		panelNorteCentral.add(xmlLoaderButton);
 		
-		panelOeste2 = new JPanel();
-		panelNorte.add(panelOeste2, BorderLayout.WEST);
+		JPanel panelCentral = new JPanel();
+		JScrollPane scrollPane = new JScrollPane(panelCentral);
+		panelCentral.setLayout(new BorderLayout(0, 0));
+		contentPane.add(scrollPane,BorderLayout.CENTER);
 		
-		panelEste2 = new JPanel();
-		panelNorte.add(panelEste2, BorderLayout.EAST);
+		JPanel panelCentralCentro = new JPanel();
+		panelCentral.add(panelCentralCentro, BorderLayout.CENTER);
+		panelCentralCentro.setLayout(new GridLayout(0, 1, 0, 8));
+		
+		
+		///////
+		
+		panel = new JPanel();
+		panelCentralCentro.add(panel);
+		panel.setLayout(new BorderLayout(0, 0));
+		
+		panel_1 = new JPanel();
+		panel.add(panel_1, BorderLayout.WEST);
+		panel_1.setLayout(new BoxLayout(panel_1, BoxLayout.X_AXIS));
+		
+		lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setIcon(new ImageIcon(VentanaPrincipal.class.getResource("/images/leopardo.png")));
+		panel_1.add(lblNewLabel_1);
+		
+		panel_2 = new JPanel();
+		panel.add(panel_2, BorderLayout.CENTER);
+		panel_2.setLayout(new BoxLayout(panel_2, BoxLayout.Y_AXIS));
+		
+		lblNewLabel_2 = new JLabel("New label");
+		panel_2.add(lblNewLabel_2);
+		
+		btnNewButton = new JButton("New button");
+		panel_2.add(btnNewButton);
+		
+		///////
+		
+//		Foto f = new Foto("Caballo", "Hola", new ArrayList<String>(), usuario.getNombre(), "/images/caballoatardecer.png");
+//		PanelItemFoto prueba = new PanelItemFoto(f);
+//		panelCentralCentro.add(prueba);
 		
 	}
 
+}
+
+class PanelItemFoto extends JPanel {
+	public PanelItemFoto (Foto f) {
+		this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+		JPanel panelFoto = new JPanel();
+		this.add(panelFoto);
+		JLabel foto = new JLabel("");
+		foto.setIcon(new ImageIcon(VentanaPrincipal.class.getResource(f.getPath())));
+		panelFoto.add(foto);
+		JPanel panelInfo = new JPanel();
+		panelInfo.setLayout(new BoxLayout(panelInfo,BoxLayout.Y_AXIS));
+		this.add(panelInfo);
+		//TODO Añadir info de la foto en panelInfo
+	}
 }
