88cd5002b13cbf83bb15bafd55b0af7d060331ab
oliatom350 <trollins030@gmail.com>
2023-04-23 12:41:17 +0200

    VentanaWarning dispose on close

--- src/main/java/tds/PhotoTDS/interfaz/VentanaWarning.java
+++ src/main/java/tds/PhotoTDS/interfaz/VentanaWarning.java
@@ -31,7 +31,7 @@ public class VentanaWarning extends JFrame {
 	}
 
 	public VentanaWarning(String out) {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 348, 256);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
