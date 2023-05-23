9f381beeb0d0ed261dfa41d15a5aea05c650ff16
oliatom350 <trollins030@gmail.com>
2023-05-23 11:56:56 +0200

    Warnings eliminados

--- src/test/java/tds/PhotoTDS/TestCalculaEdad.java
+++ src/test/java/tds/PhotoTDS/TestCalculaEdad.java
@@ -2,7 +2,6 @@ package tds.PhotoTDS;
 
 import static org.junit.Assert.*;
 
-import java.time.LocalDate;
 import java.util.Date;
 
 import org.junit.Test;
--- src/test/java/tds/PhotoTDS/mainTest.java
+++ src/test/java/tds/PhotoTDS/mainTest.java
@@ -1,23 +1,11 @@
 package tds.PhotoTDS;
 
-import java.util.Date;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-
 import tds.PhotoTDS.interfaz.VentanaEntrada;
-import tds.driver.FactoriaServicioPersistencia;
-import tds.driver.ServicioPersistencia;
 
 public class mainTest {
 
 	public static void main(String[] args) throws Exception {
 
-		Date date = new Date();  
-	    System.out.println(new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(date));  
-		
-		PhotoTDS controlador = PhotoTDS.getUnicaInstancia();
-		
-		ArrayList<Publicacion> p = (ArrayList<Publicacion>) controlador.getRepPubli().getPublicaciones();
 		VentanaEntrada vE = new VentanaEntrada();
 		vE.setVisible(true);
 
