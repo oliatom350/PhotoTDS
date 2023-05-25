7e05900b649d640d3d0f419177388c91284ac42b
Jose Antonio Sánchez Fernández <joseantonio.sanchezf1@um.es>
2023-05-25 22:15:50 +0200

    Ultima version

---
+++ src/main/java/tds/PhotoTDS/Lanzador.java
@@ -0,0 +1,12 @@
+package tds.PhotoTDS;
+
+import tds.PhotoTDS.interfaz.VentanaEntrada;
+
+public class Lanzador {
+	public static void main(String[] args) throws Exception {
+
+		VentanaEntrada vE = new VentanaEntrada();
+		vE.setVisible(true);
+
+	}
+}
